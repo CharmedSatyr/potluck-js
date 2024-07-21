e459eb0f3e25bb2bcddd0843feeb6c993828ac3d
Joseph Wolfe <joseph@WLF.to>
2024-07-21 15:00:12 -0400

    Add and run prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: true,
+	singleQuote: false,
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	trailingComma: "all",
+	bracketSpacing: true,
+	plugins: ["prettier-plugin-tailwindcss"],
+	proseWrap: "always",
+};
