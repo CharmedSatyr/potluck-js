b5a99fa53af01df03358112f5daccf4f6c890564
Joseph Wolfe <joseph@WLF.to>
2024-08-17 19:45:23 -0700

    Use uuid validation

--- src/actions/ajv.ts
+++ src/actions/ajv.ts
@@ -2,6 +2,6 @@ import Ajv from "ajv";
 import addFormats from "ajv-formats";
 
 const ajv = new Ajv();
-addFormats(ajv, ["date", "email", "iso-time"]);
+addFormats(ajv, ["date", "email", "iso-time", "uuid"]);
 
 export default ajv;
