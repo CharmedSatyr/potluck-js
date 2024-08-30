c3de6a89c461c71779134dc01d661b268c9bc49d
Joseph Wolfe <joseph@WLF.to>
2024-08-29 19:38:20 -0700

    Enable adding more dish input slots

--- src/app/start/plan-food/page.tsx
+++ src/app/start/plan-food/page.tsx
@@ -5,14 +5,97 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 interface FormInput {
-	name: string;
+	dishName: string;
 	needed: number;
 }
+
+const FoodInput = ({ register, slot }: any) => {
+	const [count, setCount] = useState<number>(1);
+
+	return (
+		<div className="flex w-full justify-between">
+			<div className="form-control w-2/3">
+				<label htmlFor="dish-name" className="label label-text">
+					What's Needed
+				</label>
+				<input
+					className="input-text input input-bordered"
+					id="dish-name"
+					type="text"
+					{...register("dishName", {
+						required: true,
+					})}
+				/>
+			</div>
+
+			<div className="form-control items-end">
+				<label htmlFor="quantity-input" className="label label-text">
+					Signups Needed
+				</label>
+				<div className="join">
+					<button
+						onClick={() => setCount(count > 1 ? count - 1 : 1)}
+						type="button"
+						className="btn join-item input-bordered"
+					>
+						<svg
+							className="h-3 w-3 text-gray-900 dark:text-white"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 18 2"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M1 1h16"
+							/>
+						</svg>
+					</button>
+					<input
+						type="text"
+						className="input join-item input-bordered w-12"
+						id="quantity-input"
+						placeholder="0"
+						value={count}
+						{...register("needed", {
+							required: true,
+						})}
+					/>
+					<button
+						onClick={() => setCount(count + 1)}
+						type="button"
+						className="btn join-item input-bordered"
+					>
+						<svg
+							className="h-3 w-3 text-gray-900 dark:text-white"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 18 18"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M9 1v16M1 9h16"
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 const PlanFoodPage = () => {
 	const { replace } = useRouter();
 	const searchParams = useSearchParams();
-	const [count, setCount] = useState<number>(0);
+	const [slots, setSlots] = useState<number>(1);
+	const { handleSubmit, register } = useForm<FormInput>({ defaultValues: {} });
 
 	const shortId = searchParams.get("event");
 
@@ -24,8 +107,6 @@ const PlanFoodPage = () => {
 		console.log("Submitting data:", data);
 	};
 
-	const { handleSubmit, register } = useForm<FormInput>({ defaultValues: {} });
-
 	return (
 		<form
 			className="form-control w-full lg:w-5/6 xl:w-2/3 2xl:w-1/2"
@@ -35,81 +116,21 @@ const PlanFoodPage = () => {
 
 			<h2>Create Your Slots</h2>
 
-			<div className="flex w-full justify-between">
-				<div className="form-control w-2/3">
-					<label htmlFor="dish-name" className="label label-text">
-						What's Needed
-					</label>
-					<input
-						className="input-text input input-bordered"
-						id="dish-name"
-						type="text"
-						{...register("name", {
-							required: true,
-						})}
-					/>
+			{Array.from({ length: slots }, (_, index) => (
+				<div key={`slot-${index}`}>
+					<FoodInput register={register} slot={index} />
+					<div className="divider mt-6" />
 				</div>
+			))}
 
-				<div className="form-control items-end">
-					<label htmlFor="quantity-input" className="label label-text">
-						Signups Needed
-					</label>
-					<div className="join">
-						<button
-							onClick={() => setCount(count > 0 ? count - 1 : 0)}
-							type="button"
-							className="btn join-item input-bordered"
-						>
-							<svg
-								className="h-3 w-3 text-gray-900 dark:text-white"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 18 2"
-							>
-								<path
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M1 1h16"
-								/>
-							</svg>
-						</button>
-						<input
-							type="text"
-							className="input join-item input-bordered w-12"
-							placeholder="0"
-							value={count}
-							{...register("needed", {
-								required: true,
-							})}
-						/>
-						<button
-							onClick={() => setCount(count + 1)}
-							type="button"
-							className="btn join-item input-bordered"
-						>
-							<svg
-								className="h-3 w-3 text-gray-900 dark:text-white"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 18 18"
-							>
-								<path
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M9 1v16M1 9h16"
-								/>
-							</svg>
-						</button>
-					</div>
-				</div>
-			</div>
-			<input className="btn btn-primary my-12" type="submit" />
+			<button
+				onClick={() => setSlots(slots < 20 ? slots + 1 : 20)}
+				className="btn btn-secondary w-1/4"
+			>
+				Add Slot
+			</button>
+
+			<input className="btn btn-primary my-8" type="submit" />
 		</form>
 	);
 };
