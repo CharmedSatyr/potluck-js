fb8e98738a1cb92db351e124d7fd977e9b0e3c22
Joseph Wolfe <joseph@WLF.to>
2024-09-03 16:12:14 -0700

    Wrap the whole app in a session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 
 import * as dotenv from "dotenv";
 import NavBar from "@/components/nav-bar";
 import siteMetadata from "@/data/site-metadata";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
+
 dotenv.config();
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,21 +17,27 @@ export const metadata: Metadata = {
 	description: siteMetadata.description,
 };
 
-export default function RootLayout({
+const RootLayout = async ({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
-}>) {
+}>) => {
+	const session = await auth();
+
 	return (
 		<html lang="en">
 			<body
 				className={`${inter.className} prose flex max-w-none flex-col items-center`}
 			>
-				<NavBar />
-				<div className="container flex w-full justify-center px-4 pt-12 md:w-10/12 md:px-10">
-					{children}
-				</div>
+				<SessionProvider session={session}>
+					<NavBar />
+					<div className="container flex w-full justify-center px-4 pt-12 md:w-10/12 md:px-10">
+						{children}
+					</div>
+				</SessionProvider>
 			</body>
 		</html>
 	);
-}
+};
+
+export default RootLayout;
