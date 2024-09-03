e25f74fdc77bba685a98b536a3b3de9054eb8521
Joseph Wolfe <joseph@WLF.to>
2024-09-02 17:08:56 -0700

    Redirect unauthed users from protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
-export const middleware = auth((req) => {
-	if (!req.auth) {
-		// DO SOMETHING
-	} else {
-		// DO SOMETHING
+const protectedRoutes = new Set(["/start/plan-food"]);
+
+export const middleware = auth((request) => {
+	if (!request.auth && protectedRoutes.has(request.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL('/', request.url))
 	}
 });
 
