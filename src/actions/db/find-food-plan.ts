8e76fe289c4d9a8ceb067ed385204cb96786ccf9
Joseph Wolfe <joseph@WLF.to>
2024-09-03 16:12:49 -0700

    Add find food plan action

---
+++ src/actions/db/find-food-plan.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { JSONSchemaType } from "ajv";
+import ajv from "@/actions/ajv";
+import validateCtx from "@/actions/validate-ctx";
+import db from "@/db/connection";
+import { foodPlan, FoodPlan } from "@/db/schema/food-plan";
+import { Party } from "@/db/schema/parties";
+import { eq } from "drizzle-orm";
+
+interface RequestData {
+	shortId: Party["shortId"];
+}
+
+const schema: JSONSchemaType<RequestData> = {
+	type: "object",
+	properties: {
+		shortId: { type: "string" },
+	},
+	required: ["shortId"],
+	additionalProperties: false,
+};
+
+const validate = ajv.compile(schema);
+
+const findFoodPlan = async (data: RequestData): Promise<FoodPlan[]> => {
+	if (!validate(data)) {
+		throw new Error(JSON.stringify(validate.errors));
+	}
+
+	const id = await validateCtx(data.shortId);
+
+	return await db.select().from(foodPlan).where(eq(foodPlan.partyId, id));
+};
+
+export default findFoodPlan;
