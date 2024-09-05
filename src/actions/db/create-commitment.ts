c8dbe6c7d9312bbfd4c12563e897f63a6e346829
Joseph Wolfe <joseph@WLF.to>
2024-09-05 14:29:48 -0700

    Add validation for new commitments

--- src/actions/db/create-commitment.ts
+++ src/actions/db/create-commitment.ts
@@ -1,43 +1,49 @@
 "use server";
 
+import { JSONSchemaType } from "ajv";
+import ajv from "@/actions/ajv";
 import { auth } from "@/auth";
 import db from "@/db/connection";
 import { Commitment, commitment } from "@/db/schema/commitment";
-import { revalidatePath } from "next/cache";
 
-interface Data {
-	description: string;
-	quantity: number;
-	foodPlanId: string;
-}
+type NewCommitment = Pick<
+	Commitment,
+	"description" | "quantity" | "foodPlanId"
+>;
 
-const createCommitment = async ({
-	description,
-	quantity,
-	foodPlanId,
-}: Data): Promise<Commitment[]> => {
+const schema: JSONSchemaType<NewCommitment> = {
+	type: "object",
+	properties: {
+		description: { type: "string" },
+		foodPlanId: { type: "string" },
+		quantity: { type: "number" },
+	},
+	required: ["description", "foodPlanId", "quantity"],
+	additionalProperties: false,
+};
+
+const validate = ajv.compile(schema);
+
+const createCommitment = async (data: NewCommitment): Promise<Commitment[]> => {
+	if (!validate(data)) {
+		throw new Error(JSON.stringify(validate.errors));
+	}
+
 	const session = await auth();
 
-	const createdBy = session?.user?.email;
-	const avatar = session?.user?.image ?? "";
-
-	if (!createdBy) {
+	if (!session?.user?.email || !session.user.image) {
 		throw new Error("Not authenticated");
 	}
 
 	const values = {
-		avatar,
-		createdBy,
-		description,
-		quantity,
-		foodPlanId,
+		avatar: session.user.image,
+		createdBy: session.user.email,
+		description: data.description,
+		quantity: data.quantity,
+		foodPlanId: data.foodPlanId,
 	};
 
-	const result = await db.insert(commitment).values(values).returning();
-
-	revalidatePath(`/`, "layout"); // TODO: DOn't do this. Just the one page.
-
-	return result;
+	return await db.insert(commitment).values(values).returning();
 };
 
 export default createCommitment;
