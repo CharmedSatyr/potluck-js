1c95ef8af777f00b6abe9c25c7230ec815c5230c
Joseph Wolfe <joseph@WLF.to>
2024-09-05 17:35:56 -0700

    Remove dishes

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,14 +1,16 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { dishes } from "@/db/schema/dishes";
 import { parties } from "@/db/schema/parties";
 import { config } from "@/db/config";
+import { commitment } from "./schema/commitment";
+import { foodPlan } from "./schema/food-plan";
 
 const sql = neon(config.connectionString);
 
 const schema = {
-	dishes,
 	parties,
+	commitment,
+	foodPlan,
 };
 
 const db = drizzle(sql, { schema });
