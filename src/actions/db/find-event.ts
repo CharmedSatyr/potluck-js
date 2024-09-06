af9d2a9b6c8d3e756879ec5e4f642f34cdcb4a8a
Joseph Wolfe <joseph@WLF.to>
2024-09-06 06:37:10 -0700

    Standardize find actions

--- src/actions/db/find-event.ts
+++ src/actions/db/find-event.ts
@@ -1,9 +1,34 @@
 "use server";
 
+import { JSONSchemaType } from "ajv";
+import ajv from "@/actions/ajv";
 import db from "@/db/connection";
-import { Event } from "@/db/schema/event";
+import { Event, event } from "@/db/schema/event";
+import { eq } from "drizzle-orm";
 
-const findEvent = async (eventCode: string): Promise<Event | undefined> =>
-	await db.query.event.findFirst({ with: { eventCode } });
+interface EventCode {
+	code: Event["code"];
+}
+
+const schema: JSONSchemaType<EventCode> = {
+	type: "object",
+	properties: {
+		code: { type: "string" },
+	},
+	required: ["code"],
+	additionalProperties: false,
+};
+
+const validate = ajv.compile(schema);
+
+const findEvent = async ({ code }: EventCode): Promise<Event | undefined> => {
+	if (!validate({ code })) {
+		throw new Error(JSON.stringify(validate.errors));
+	}
+
+	return await db.query.event.findFirst({
+		where: eq(event.code, code),
+	});
+};
 
 export default findEvent;
