5310eebe8a19d2d7eafa8935d101b3254f7f5cdd
Joseph Wolfe <joseph@WLF.to>
2024-09-05 18:33:52 -0700

    Simplify output

--- src/actions/db/find-commitments.ts
+++ src/actions/db/find-commitments.ts
@@ -4,34 +4,34 @@ import db from "@/db/connection";
 import { foodPlan } from "@/db/schema/food-plan";
 import { eq } from "drizzle-orm";
 import { commitment, Commitment } from "@/db/schema/commitment";
+import { Party } from "@/db/schema/parties";
 
-interface RequestData {
-	partyId: string;
-}
+const findCommitments = async (partyId: Party["id"]): Promise<Commitment[]> => {
+	const {
+		avatar,
+		createdAt,
+		createdBy,
+		description,
+		foodPlanId,
+		id,
+		quantity,
+		updatedAt,
+	} = commitment;
 
-export interface Return {
-	[foodPlanId: string]: Commitment[];
-}
-
-const findCommitments = async ({ partyId }: RequestData): Promise<Return> => {
-	const result = await db
-		.select({ commitment })
+	return await db
+		.select({
+			avatar,
+			createdAt,
+			createdBy,
+			description,
+			foodPlanId,
+			id,
+			quantity,
+			updatedAt,
+		})
 		.from(foodPlan)
 		.where(eq(foodPlan.partyId, partyId))
 		.innerJoin(commitment, eq(foodPlan.id, commitment.foodPlanId));
-
-	return result
-		.map((item) => ({ ...item.commitment }))
-		.reduce<Return>((acc, curr) => {
-			if (acc[curr.foodPlanId]) {
-				acc[curr.foodPlanId] = [...acc[curr.foodPlanId], curr];
-
-				return acc;
-			}
-
-			acc[curr.foodPlanId] = [curr];
-			return acc;
-		}, {});
 };
 
 export default findCommitments;
