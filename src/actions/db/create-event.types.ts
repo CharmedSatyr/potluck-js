8cb950c19796e0b384e72ad2cbf6a26eb59c7145
Joseph Wolfe <joseph@WLF.to>
2024-09-20 08:52:21 -0700

    Trim inputs

--- src/actions/db/create-event.types.ts
+++ src/actions/db/create-event.types.ts
@@ -7,18 +7,19 @@ futureDate.setFullYear(futureDate.getFullYear() + 1);
 
 export const schema: z.ZodType<CustomizableEventValues> = z
 	.strictObject({
-		description: z.string(),
-		hosts: z.string(),
-		location: z.string().min(1, { message: "Location required." }),
-		name: z.string().min(1, { message: "Name required." }),
+		description: z.string().trim(),
+		hosts: z.string().trim(),
+		location: z.string().trim().min(1, { message: "Location required." }),
+		name: z.string().trim().min(1, { message: "Name required." }),
 		startDate: z
 			.string({ message: "Date required." })
+			.trim()
 			.date("Date must be valid.")
 			.refine(
 				(val) => new Date(val) >= currentDate && new Date(val) <= futureDate,
 				{ message: "Date must be within the next year." }
 			),
-		startTime: z.string().time({ message: "Time required." }),
+		startTime: z.string().trim().time({ message: "Time required." }),
 	})
 	.required();
 
