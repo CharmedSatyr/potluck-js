59509ff034e8eb0ef958c02ba411bcdcf8a59fac
Joseph Wolfe <joseph@WLF.to>
2024-09-27 11:39:41 -0700

    Standardize update event logic

--- src/actions/db/update-event.ts
+++ src/actions/db/update-event.ts
@@ -1,59 +1,30 @@
 "use server";
 
-import { JSONSchemaType } from "ajv";
+import { z } from "zod";
 import { eq } from "drizzle-orm";
-import ajv from "@/actions/ajv";
 import db from "@/db/connection";
-import { Event, event, CustomizableEventValues } from "@/db/schema/event";
+import { Event, event } from "@/db/schema/event";
 import findEvent from "@/actions/db/find-event";
-import { auth } from "@/auth";
-
-export type UpdatedEvent = Pick<Event, "code"> &
-	Partial<CustomizableEventValues>;
-
-// TODO: Enforce that at least one updated property is included
-const schema: JSONSchemaType<UpdatedEvent> = {
-	type: "object",
-	properties: {
-		code: { type: "string" },
-		description: { type: "string", nullable: true },
-		hosts: { type: "string", nullable: true },
-		location: { type: "string", nullable: true },
-		name: { type: "string", nullable: true },
-		startDate: { type: "string", format: "date", nullable: true },
-		startTime: { type: "string", format: "iso-time", nullable: true },
-	},
-	required: ["code"],
-	additionalProperties: false,
-};
-
-const validate = ajv.compile(schema);
+import { schema } from "@/actions/db/update-event.types";
 
 const updateEvent = async (
-	updatedEvent: UpdatedEvent
+	data: z.infer<typeof schema>
 ): Promise<{ code: Event["code"] }[]> => {
-	if (!validate(updatedEvent)) {
-		throw new Error(JSON.stringify(validate.errors));
+	try {
+		schema.parse(data);
+
+		const [eventToUpdate] = await findEvent({ code: data.code });
+
+		return await db
+			.update(event)
+			.set(data)
+			.where(eq(event.id, eventToUpdate.id))
+			.returning({ code: event.code });
+	} catch (err) {
+		console.error(err);
+
+		return [];
 	}
-
-	const { code, ...values } = updatedEvent;
-	const [eventToUpdate] = await findEvent({ code });
-
-	if (!eventToUpdate) {
-		throw new Error("Invalid code");
-	}
-
-	const session = await auth();
-
-	if (!session?.user?.email) {
-		throw new Error("Not authenticated");
-	}
-
-	return await db
-		.update(event)
-		.set({ ...values })
-		.where(eq(event.id, eventToUpdate.id))
-		.returning({ code: event.code });
 };
 
 export default updateEvent;
---
+++ src/actions/db/update-event.types.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import { Event, EVENT_CODE_LENGTH, EventUserValues } from "@/db/schema/event";
+import formatIsoTime from "@/utilities/format-iso-time";
+
+const currentDate = new Date();
+const futureDate = new Date(currentDate);
+futureDate.setFullYear(futureDate.getFullYear() + 1);
+
+export const schema = z.strictObject({
+	code: z.string().trim().length(EVENT_CODE_LENGTH),
+	createdBy: z.string().trim().uuid(),
+	description: z.string().trim().max(256).optional(),
+	hosts: z.string().trim().max(256).optional(),
+	location: z
+		.string()
+		.trim()
+		.min(1, { message: "Location required." })
+		.max(256)
+		.optional(),
+	name: z
+		.string()
+		.trim()
+		.min(1, { message: "Name required." })
+		.max(256)
+		.optional(),
+	startDate: z
+		.string({ message: "Date required." })
+		.trim()
+		.date("Date must be valid.")
+		.refine(
+			(val) => new Date(val) >= currentDate && new Date(val) <= futureDate,
+			{ message: "Date must be within the next year." }
+		)
+		.optional(),
+	startTime: z
+		.string()
+		.transform(formatIsoTime)
+		// Can't use `.time()` method with transform.
+		.refine((val) => /^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/.test(val), {
+			message: "Time required.",
+		})
+		.optional(),
+}) satisfies z.ZodType<
+	Partial<EventUserValues> & Required<Pick<Event, "code" | "createdBy">>
+>;
