234b053e53ea197f1d199314b1764f95a23f0779
Joseph Wolfe <joseph@WLF.to>
2024-09-26 20:26:36 -0700

    Upgrade loading indicator styles

--- src/components/loading-indicator.tsx
+++ src/components/loading-indicator.tsx
@@ -1,5 +1,8 @@
 const LoadingIndicator = () => (
-	<span className="loading loading-ring loading-lg"></span>
+	<>
+		<span className="loading loading-ring loading-lg absolute z-50 h-72 w-72 text-accent" />
+		<div className="fixed inset-0 bg-black bg-opacity-50" />
+	</>
 );
 
 export default LoadingIndicator;
