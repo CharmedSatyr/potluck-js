57097d1da2465abc0f2677e113affecfe4581dea
Joseph Wolfe <joseph@WLF.to>
2024-10-11 06:30:28 -0700

    Coerce number; include createdBy

--- src/actions/db/create-commitment.types.ts
+++ src/actions/db/create-commitment.types.ts
@@ -5,9 +5,9 @@ export const schema = z
 	.strictObject({
 		createdBy: z.string().trim().uuid(),
 		description: z.string().trim().max(256),
-		quantity: z.number().positive().max(256),
+		quantity: z.coerce.number().positive().max(256),
 		requestId: z.string().trim().uuid(),
 	})
 	.required() satisfies z.ZodType<
-	Pick<Commitment, "description" | "quantity" | "requestId">
+	Pick<Commitment, "createdBy" | "description" | "quantity" | "requestId">
 >;
