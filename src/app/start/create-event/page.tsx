088391e42b673069387bf9734336473d52552039
Joseph Wolfe <joseph@WLF.to>
2024-10-29 22:32:45 -0700

    Replace loading indicator with skeleton

--- src/app/start/create-event/page.tsx
+++ src/app/start/create-event/page.tsx
@@ -1,11 +1,24 @@
 import { Suspense } from "react";
 import CreateEventManager from "@/app/start/create-event/create-event-manager";
-import LoadingIndicator from "@/components/loading-indicator";
+
+const LoadingSkeleton = () => {
+	return (
+		<>
+			<div className="skeleton mb-4 mt-2 h-10 w-full"></div>
+			<div className="skeleton my-2 h-14 w-full"></div>
+			<div className="skeleton my-2 h-8 w-full"></div>
+			<div className="skeleton my-2 h-8 w-full"></div>
+			<div className="skeleton my-2 h-8 w-full"></div>
+			<div className="skeleton my-2 h-8 w-full"></div>
+			<div className="skeleton my-4 h-14 w-full"></div>
+		</>
+	);
+};
 
 const CreateEventPage = async () => {
 	return (
 		<div className="flex w-full flex-col items-center justify-items-center">
-			<Suspense fallback={<LoadingIndicator />}>
+			<Suspense fallback={<LoadingSkeleton />}>
 				<CreateEventManager />
 			</Suspense>
 
