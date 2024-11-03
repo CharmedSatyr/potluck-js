ad60312ab613ad62924542b67f77376de8cd3fe8
Joseph Wolfe <joseph@WLF.to>
2024-11-02 19:37:02 -0700

    Add use-anchor hook

---
+++ src/hooks/use-anchor.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const useAnchor = (): [string] => {
+    const [mounted, setMounted] = useState<boolean>(false);
+    const [anchor, setAnchor] = useState<string>("");
+    useEffect(() => {
+        if (mounted) {
+            return;
+        }
+
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (!mounted) {
+            return;
+        }
+
+        if (typeof window === 'undefined') {
+            return;
+        }
+
+        const handleAnchorChange = (event: HashChangeEvent) => {
+            setAnchor(event.newURL)
+        }
+
+        window.addEventListener("hashchange", handleAnchorChange);
+
+        return () => window.removeEventListener("hashchange", handleAnchorChange);
+    }, [mounted]);
+
+    return [anchor.split("#")[1]];
+}
+
+
+export default useAnchor;
