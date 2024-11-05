0c92241310b17105a70a6186cb21c7983f755152
Joseph Wolfe <joseph@WLF.to>
2024-11-04 19:03:19 -0800

    Use /start for event creation

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,5 +1,16 @@
-import { redirect } from "next/navigation";
+"use client";
 
-const StartPage = () => redirect("/start/create-event");
+import { Suspense } from "react";
+import ManageEventWizard from "@/components/manage-event-wizard";
+
+const StartPage = () => {
+	return (
+		<div className="flex h-full w-full flex-col items-center">
+			<Suspense>
+				<ManageEventWizard />
+			</Suspense>
+		</div>
+	);
+};
 
 export default StartPage;
