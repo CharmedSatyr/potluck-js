b81f2b98249d96a29c320f1a5e26a06449ff1ef5
Joseph Wolfe <joseph@WLF.to>
2024-11-15 20:45:43 -0800

    Extract warning alert component

--- src/components/plan-event-form/index.tsx
+++ src/components/plan-event-form/index.tsx
@@ -7,6 +7,7 @@ import {
 	PlanEventFormData,
 	PlanEventFormState,
 } from "@/app/start/submit-actions.schema";
+import WarningAlert from "../warning-alert";
 
 type Props = {
 	code: string | null;
@@ -15,32 +16,6 @@ type Props = {
 		prevState: PlanEventFormState,
 		formData: FormData
 	) => Promise<PlanEventFormState>;
-};
-
-const FormErrorAlert = ({ text }: { text: string }) => {
-	if (!text) {
-		return null;
-	}
-
-	return (
-		<div role="alert" className="alert mt-2 py-1 text-warning">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				className="h-6 w-6 shrink-0 stroke-current"
-				fill="none"
-				viewBox="0 0 24 24"
-			>
-				<path
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					strokeWidth="2"
-					d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-				/>
-			</svg>
-
-			<span>{text}</span>
-		</div>
-	);
 };
 
 const PlanEventForm = ({ code, eventDataPromise, submitAction }: Props) => {
@@ -116,7 +91,7 @@ const PlanEventForm = ({ code, eventDataPromise, submitAction }: Props) => {
 					type="text"
 				/>
 				{state?.errors?.fieldErrors.name && (
-					<FormErrorAlert text={state?.errors?.fieldErrors.name?.join(" ")} />
+					<WarningAlert text={state?.errors?.fieldErrors.name?.join(" ")} />
 				)}
 			</div>
 
@@ -135,7 +110,7 @@ const PlanEventForm = ({ code, eventDataPromise, submitAction }: Props) => {
 						type="date"
 					/>
 					{state?.errors?.fieldErrors.startDate && (
-						<FormErrorAlert
+						<WarningAlert
 							text={state?.errors?.fieldErrors.startDate?.join(" ")}
 						/>
 					)}
@@ -156,7 +131,7 @@ const PlanEventForm = ({ code, eventDataPromise, submitAction }: Props) => {
 						type="time"
 					/>
 					{state?.errors?.fieldErrors.startTime && (
-						<FormErrorAlert
+						<WarningAlert
 							text={state?.errors?.fieldErrors.startTime?.join(" ")}
 						/>
 					)}
@@ -178,9 +153,7 @@ const PlanEventForm = ({ code, eventDataPromise, submitAction }: Props) => {
 					type="text"
 				/>
 				{state?.errors?.fieldErrors.location && (
-					<FormErrorAlert
-						text={state?.errors?.fieldErrors.location?.join(" ")}
-					/>
+					<WarningAlert text={state?.errors?.fieldErrors.location?.join(" ")} />
 				)}
 			</div>
 
@@ -202,7 +175,7 @@ const PlanEventForm = ({ code, eventDataPromise, submitAction }: Props) => {
 				</div>
 
 				{state?.errors?.fieldErrors.hosts && (
-					<FormErrorAlert text={state?.errors?.fieldErrors.hosts?.join(" ")} />
+					<WarningAlert text={state?.errors?.fieldErrors.hosts?.join(" ")} />
 				)}
 			</div>
 
@@ -223,7 +196,7 @@ const PlanEventForm = ({ code, eventDataPromise, submitAction }: Props) => {
 					/>
 				</div>
 				{state?.errors?.fieldErrors.description && (
-					<FormErrorAlert
+					<WarningAlert
 						text={state?.errors?.fieldErrors.description?.join(" ")}
 					/>
 				)}
