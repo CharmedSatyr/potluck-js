8b7694f7c9b171113e9ec3333cbd01f8955d3ca6
Joseph Wolfe <joseph@WLF.to>
2024-11-16 11:51:28 -0800

    Make text stand out more

--- src/components/copy-link-button.tsx
+++ src/components/copy-link-button.tsx
@@ -42,7 +42,7 @@ const CopyLinkButton = ({ className, text }: Props) => {
 	return (
 		<div className="tooltip" data-tip={clicked ? "Copied" : "Copy Link"}>
 			<button
-				className="btn btn-ghost btn-sm text-lg font-bold px-2"
+				className="btn btn-ghost btn-sm px-2 text-lg font-bold"
 				onClick={copyWithReset}
 				type="button"
 			>
