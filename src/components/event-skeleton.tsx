e2c2999e8d0c563869853ca7918fb0feefc57d77
Joseph Wolfe <joseph@WLF.to>
2024-11-19 11:15:33 -0800

    Expect authenticated state

--- src/components/event-skeleton.tsx
+++ src/components/event-skeleton.tsx
@@ -17,9 +17,18 @@ type Props = {
 	code: string;
 };
 
-const EventSkeleton = async ({ code }: Props) => {
-	const session = await auth();
+export const EventHeader = ({ code, name }: { code: string; name: string }) => {
+	return (
+		<>
+			<h1 className="mb-4 text-6xl font-bold text-primary">{name}</h1>
+			<div className="font-bold">
+				Event Code: <CopyLinkButton text={code} />
+			</div>
+		</>
+	);
+};
 
+const EventSkeleton = async ({ code }: Props) => {
 	const [event] = await findEvent({ code });
 	const [creator] = await findUserByEventCode({ code });
 
@@ -28,41 +37,29 @@ const EventSkeleton = async ({ code }: Props) => {
 	const isPassed = eventIsPassed(startDate);
 
 	return (
-		<div className="flex w-full justify-between">
-			<div className="max-w-md">
-				<h1 className="mb-4 text-5xl font-bold text-primary">{name}</h1>
-				<div className="font-bold">
-					Event Code: <CopyLinkButton text={code} />
-				</div>
+		<div className="w-full">
+			<EventHeader code={code} name={name} />
 
-				{!session?.user?.id ? (
-					<p>Sign In to see all the details!</p>
-				) : (
-					<>
-						<p className="flex items-center gap-2">
-							<CalendarIcon className="h-4 w-4" /> {formatStartDate(startDate)}{" "}
-							at <ClockIcon className="h-4 w-4" /> {formatStartTime(startTime)}
-						</p>
-						<p className="flex items-center gap-2">
-							<MapPinIcon className="h-4 w-4" /> {location}
-						</p>
-						<p className="flex h-6 items-center gap-2">
-							<Image
-								alt={`${creator.name}'s Avatar`}
-								className="avatar rounded-full border"
-								src={creator.image!}
-								height="20"
-								width="20"
-							/>
-							Hosted by {hosts}
-						</p>
-						<p>{description}</p>
-						{isPassed && (
-							<WarningAlert text="This event takes place in the past." />
-						)}
-					</>
-				)}
-			</div>
+			<p className="flex items-center gap-2">
+				<CalendarIcon className="h-4 w-4" /> {formatStartDate(startDate)} at{" "}
+				<ClockIcon className="h-4 w-4" /> {formatStartTime(startTime)}
+			</p>
+			<p className="flex items-center gap-2">
+				<MapPinIcon className="h-4 w-4" /> {location}
+			</p>
+			<p className="flex h-6 items-center gap-2">
+				<Image
+					alt={`${creator.name}'s Avatar`}
+					className="avatar rounded-full border"
+					src={creator.image!}
+					height="20"
+					width="20"
+				/>
+				Hosted by {hosts}
+			</p>
+			<p>{description}</p>
+
+			{isPassed && <WarningAlert text="This event takes place in the past." />}
 		</div>
 	);
 };
