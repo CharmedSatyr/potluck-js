8e32f0e7776e29228186980e2615624ebd594b6e
Joseph Wolfe <joseph@WLF.to>
2024-11-19 18:04:21 -0800

    Add Discord icon to sign in button

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,6 +1,7 @@
 import Form from "next/form";
 import signInWithDiscordAndRevalidate from "@/actions/auth/sign-in-with-discord-and-revalidate";
 import { auth } from "@/auth";
+import { DiscordIcon } from "./icons/discord";
 
 const SignIn = async () => {
 	const session = await auth();
@@ -12,7 +13,7 @@ const SignIn = async () => {
 	return (
 		<Form action={signInWithDiscordAndRevalidate}>
 			<button className="btn btn-accent btn-sm" type="submit">
-				Sign In with Discord
+				Sign In <DiscordIcon height="16" width="16" />
 			</button>
 		</Form>
 	);
