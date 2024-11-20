c8552bf91ad1f575f86a86e33824a0475a4be891
Joseph Wolfe <joseph@WLF.to>
2024-11-19 18:34:25 -0800

    Use custom theme and logo image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,39 @@
 import Link from "next/link";
 import GotoEventForm from "@/components/goto-event-form";
 import siteMetadata from "@/data/site-metadata";
+import Image from "next/image";
 
 const Home = () => {
 	return (
 		<main>
-			<h1 className="mb-2">{siteMetadata.title}</h1>
-			<h2 className="mt-0 text-secondary">a potluck planning app</h2>
+			<div className="hero min-h-full bg-base-200">
+				<div className="hero-content mb-16 flex-col lg:flex-row-reverse">
+					<Image
+						alt="Potluck Quest logo"
+						className="max-w-sm rounded-lg shadow-2xl"
+						src="/static/potluck-quest.png"
+						height="500"
+						width="500"
+					/>
+					<div className="px-4">
+						<h1 className="text-5xl mt-8 font-bold leading-normal">Gather your party, and roll for dinner!</h1>
+						<p>
+							<span className="text-primary">Potluck Quest</span> makes it easy for you and your friends to plan
+							shared meals. In development by and for tabletop gamers who enjoy cooking and eating with their friends.
+						</p>
 
-			<div className="mt-20 flex min-h-60 w-full justify-center">
-				<div className="divider divider-start divider-horizontal w-fit">
-					<Link href="/start" className="btn btn-primary text-2xl">
-						<button type="button">Create an Event</button>
-					</Link>
-				</div>
-				<div className="divider divider-horizontal">OR</div>
-				<div className="divider divider-end divider-horizontal w-fit">
-					<GotoEventForm />
+						<div className="mt-12 flex min-h-60 w-full justify-center">
+							<div className="divider divider-start divider-horizontal w-fit">
+								<Link href="/start" className="btn btn-primary text-2xl">
+									<button type="button">Create an Event</button>
+								</Link>
+							</div>
+							<div className="divider divider-horizontal">OR</div>
+							<div className="divider divider-end divider-horizontal w-fit">
+								<GotoEventForm />
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,21 @@ const config: Config = {
 		},
 	},
 	daisyui: {
-		themes: ["dracula"],
+		themes: [
+			{
+				potluck: {
+					primary: "#FF8A50", // Slightly muted orange for vibrancy on dark
+					secondary: "#4CAF50", // Deep green with good contrast
+					accent: "#FFB74D", // Rich golden-yellow for highlights
+					neutral: "#3E2723", // Dark brown for a warm neutral base
+					"base-100": "#212121", // Dark gray for background
+					info: "#1E88E5", // Deep blue for informational tones
+					success: "#66BB6A", // Calm green for success indicators
+					warning: "#FFB300", // Bold amber for warnings
+					error: "#D32F2F", // Deep red for errors
+				},
+			},
+		],
 	},
 	plugins: [typography, daisyui],
 };
