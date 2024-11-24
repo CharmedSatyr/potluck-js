3a56a9fa14403fde5d9e1e3d0c6e9bd34da7dd0c
Joseph Wolfe <joseph@WLF.to>
2024-11-24 18:47:18 -0500

    Responsiveness improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,53 @@ import GotoEventForm from "@/components/goto-event-form";
 import siteMetadata from "@/data/site-metadata";
 import Image from "next/image";
 
+const CreateEventButton = () => (
+	<Link href="/start" className="btn btn-primary text-xl">
+		Create an Event
+	</Link>
+);
+
+const StartCta = () => {
+	return (
+		<div className="w-full xl:p-10">
+			<div className="flex flex-col gap-2 text-center md:hidden">
+				<CreateEventButton />
+
+				<div className="divider-base divider">OR</div>
+
+				<GotoEventForm />
+			</div>
+
+			<div className="hidden min-h-44 w-full md:flex">
+				<div className="divider divider-start divider-horizontal w-1/2">
+					<CreateEventButton />
+				</div>
+				<div className="divider divider-horizontal">OR</div>
+				<div className="divider divider-end divider-horizontal w-1/2">
+					<GotoEventForm />
+				</div>
+			</div>
+		</div>
+	);
+};
+
 const Home = () => {
 	return (
-		<main className="-z-10 xl:grid xl:grid-cols-2 xl:place-items-end xl:bg-base-300 xl:p-10">
-			<div className="h-full">
+		<main className="-z-10 rounded-xl bg-base-300 py-10 xl:flex">
+			<div className="h-full xl:h-1/2">
 				<div className="hero">
-					<div className="hero-content flex-col rounded-lg text-center">
-						<h1 className="mb-0 text-5xl font-bold leading-tight">
+					<div className="hero-content flex-col rounded-xl text-center xl:mx-10">
+						<h1 className="mb-0 text-4xl font-bold leading-tight sm:text-5xl">
 							Gather your party, and roll for an epic meal.
 						</h1>
+
+						<p className="mb-0">
+							<span className="text-primary">{siteMetadata.title}</span> makes
+							it easy for you and your friends to plan shared meals.{" "}
+							<span className="text-info">In active development</span> by and
+							for tabletop gamers who enjoy cooking and eating with their
+							friends.
+						</p>
 
 						<div className="flex w-full justify-center xl:hidden">
 							<Image
@@ -23,29 +61,13 @@ const Home = () => {
 							/>
 						</div>
 
-						<p>
-							<span className="text-primary">{siteMetadata.title}</span> makes
-							it easy for you and your friends to plan shared meals.{" "}
-							<span className="text-info">In active development</span> by and
-							for tabletop gamers who enjoy cooking and eating with their
-							friends.
-						</p>
+						<StartCta />
 					</div>
-				</div>
-
-				<div className="flex flex-col gap-2 text-center">
-					<Link href="/start" className="btn btn-primary w-full text-xl">
-						<button className="h-full w-full" type="button">
-							Create an Event
-						</button>
-					</Link>
-					<div className="divider-base divider">OR</div>
-					<GotoEventForm />
 				</div>
 			</div>
 
-			<div className="hidden xl:flex xl:h-full xl:w-full xl:items-center xl:justify-center">
-				<div className="h-96 w-96 xl:relative">
+			<div className="hidden h-full w-full items-center justify-center xl:flex 2xl:justify-center">
+				<div className="relative h-80 w-80 2xl:h-96 2xl:w-96">
 					<Image
 						alt={`${siteMetadata.title} logo`}
 						className="m-0 rounded-lg shadow-2xl"
