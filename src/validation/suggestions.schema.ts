cd72350aca9319ffe86644f0ed58467104672549
Joseph Wolfe <joseph@WLF.to>
2024-11-29 15:55:47 -0600

    Add descriptions

--- src/validation/suggestions.schema.ts
+++ src/validation/suggestions.schema.ts
@@ -1,11 +1,18 @@
 import { z } from "zod";
 
 export const suggestions = z.strictObject({
-	advice: z.string().trim().max(500),
-	dishes: z.array(
-		z.object({
-			type: z.string().trim().max(250),
-			count: z.number().max(999),
-		})
-	),
+	advice: z.string().trim().max(500).describe(`
+			A helpful, unformatted message indicating an approach to meal planning
+			for this event given the particular event details, and themed for Potluck Quest.
+		`),
+	items: z
+		.array(
+			z.strictObject({
+				type: z.string().trim().max(250),
+				count: z.number().max(999),
+			})
+		)
+		.describe(
+			"A list of types and counts of items the host should request from attendees."
+		),
 });
