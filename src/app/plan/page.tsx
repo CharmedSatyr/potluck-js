9e03eabfbea6af38617e4631d640457c36f695b8
Joseph Wolfe <joseph@WLF.to>
2024-12-02 08:31:55 -0800

    Restore error boundary

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -5,6 +5,7 @@ import {
 	buildEventDataFromParams,
 	buildSlotDataFromParams,
 } from "@/utilities/build-data-from-params";
+import ErrorBoundary from "@/components/error-boundary";
 
 type Props = {
 	searchParams: Promise<{ [key: string]: string }>;
@@ -19,15 +20,17 @@ const PlanPage = async ({ searchParams }: Props) => {
 
 	return (
 		<main className="flex h-full w-full flex-col items-center">
-			<Suspense fallback={<PlanEventFormFallback />}>
-				<ManageEventWizard
-					code={code}
-					committedUsersBySlotPromise={Promise.resolve(new Map())}
-					eventDataPromise={Promise.resolve([eventData])}
-					mode="create"
-					slotsPromise={Promise.resolve(slotData)}
-				/>
-			</Suspense>
+			<ErrorBoundary>
+				<Suspense fallback={<PlanEventFormFallback />}>
+					<ManageEventWizard
+						code={code}
+						committedUsersBySlotPromise={Promise.resolve(new Map())}
+						eventDataPromise={Promise.resolve([eventData])}
+						mode="create"
+						slotsPromise={Promise.resolve(slotData)}
+					/>
+				</Suspense>
+			</ErrorBoundary>
 		</main>
 	);
 };
