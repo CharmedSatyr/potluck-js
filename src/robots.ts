bb8efa91b7a2ed8cd25e5fca400a50223d7c76b7
Joseph Wolfe <joseph@WLF.to>
2024-12-05 10:17:51 -0800

    Add robots, seo, and sitemap files

---
+++ src/robots.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from "next";
+
+import siteMetadata from "@/data/site-metadata";
+
+const robots = (): MetadataRoute.Robots => {
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+		},
+		host: siteMetadata.siteUrl,
+	};
+};
+
+export default robots;
