4808a670e62b9a4bf0c5b09e5aaf40f9d25d6b0f
Joseph Wolfe <joseph@WLF.to>
2024-12-19 17:20:17 -0800

    Standardize zod schemas for event fields

--- src/actions/db/create-event.schema.ts
+++ src/actions/db/create-event.schema.ts
@@ -1,36 +1,21 @@
 import { z } from "zod";
 import { EventUserValues } from "@/db/schema/event";
-import formatIsoTime from "@/utilities/format-iso-time";
-
-const currentDate = new Date();
-const futureDate = new Date(currentDate);
-futureDate.setFullYear(futureDate.getFullYear() + 1);
+import { createdBy } from "@/validation/createdBy.schema";
+import { location } from "@/validation/location.schema";
+import { title } from "@/validation/title.schema";
+import { description } from "@/validation/description.schema";
+import { hosts } from "@/validation/hosts.schema";
+import { startDate } from "@/validation/startDate.schema";
+import { startTime } from "@/validation/startTime.schema";
 
 export const schema = z
 	.strictObject({
-		createdBy: z.string().trim().uuid(),
-		description: z.string().trim().max(256),
-		hosts: z.string().trim().max(256),
-		location: z
-			.string()
-			.trim()
-			.min(1, { message: "Location required." })
-			.max(256),
-		startDate: z
-			.string({ message: "Date required." })
-			.trim()
-			.date("Date must be valid.")
-			.refine(
-				(val) => new Date(val) >= currentDate && new Date(val) <= futureDate,
-				{ message: "Date must be within the next year." }
-			),
-		startTime: z
-			.string()
-			.transform(formatIsoTime)
-			// Can't use `.time()` method with transform.
-			.refine((val) => /^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/.test(val), {
-				message: "Time required.",
-			}),
-		title: z.string().trim().min(1, { message: "Title required." }).max(100),
+		createdBy,
+		description,
+		hosts,
+		location,
+		startDate,
+		startTime,
+		title,
 	})
 	.required() satisfies z.ZodType<EventUserValues>;
