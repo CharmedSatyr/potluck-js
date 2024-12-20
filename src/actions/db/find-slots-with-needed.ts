f32ff0fd064c77c7d7c97956358ef0221ac5d8a8
Joseph Wolfe <joseph@WLF.to>
2024-12-19 21:31:45 -0800

    Add slot id to response

--- src/actions/db/find-slots-with-needed.ts
+++ src/actions/db/find-slots-with-needed.ts
@@ -4,12 +4,13 @@ import { z } from "zod";
 import { count, eq, sql } from "drizzle-orm";
 import db from "@/db/connection";
 import findEvent from "@/actions/db/find-event";
-import { slot } from "@/db/schema/slot";
+import { Slot, slot } from "@/db/schema/slot";
 import { schema } from "@/actions/db/find-slots-with-needed.schema";
 import { commitment } from "@/db/schema/commitment";
 
 type SlotsWithNeeded = {
-	item: string;
+	id: Slot["id"];
+	item: Slot["item"];
 	needed: number;
 };
 
@@ -27,6 +28,7 @@ const findSlotsWithNeeded = async (
 
 		return await db
 			.select({
+				id: slot.id,
 				item: slot.item,
 				needed: sql<number>`cast(${slot.count} - ${count(commitment)} as int)`,
 			})
