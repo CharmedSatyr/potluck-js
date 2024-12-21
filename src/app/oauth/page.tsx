c84a8591da0fe6a3db4fe25b1e7cd6c420afdd4f
Joseph Wolfe <joseph@WLF.to>
2024-12-20 17:59:36 -0800

    Simplify login page

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,37 +1,22 @@
-
-
-
 import Form from "next/form";
-import { signIn } from "@/auth";
 import { DiscordIcon } from "@/components/icons/discord";
+import signInWithDiscordAndRevalidate from "@/actions/auth/sign-in-with-discord-and-revalidate";
 
 type Props = {
-    params: Promise<{ code: string }>;
-    searchParams: Promise<{ [key: string]: string }>;
+	params: Promise<{ code: string }>;
+	searchParams: Promise<{ [key: string]: string }>;
 };
 
 const OauthPage = async ({ params, searchParams }: Props) => {
-    return (
-        <Form
-            action={async () => {
-                "use server";
-                //               const search = await searchParams;
+	return (
+		<Form action={signInWithDiscordAndRevalidate}>
+			<h1>Please sign in.</h1>
 
-                // await signIn("discord")//, {
-                /*
-                redirectTo: `/event/${code}/edit`
-                    .concat("?")
-                    .concat(new URLSearchParams(search).toString()),
-            });
-            */
-            }}
-        >
-            <h1>Please sign in.</h1>
-            <button className="btn btn-primary my-6 w-full" type="submit">
-                Continue with Discord <DiscordIcon className="size-4" />
-            </button>
-        </Form>
-    );
-}
+			<button className="btn btn-primary my-6 w-full" type="submit">
+				Continue with Discord <DiscordIcon className="size-4" />
+			</button>
+		</Form>
+	);
+};
 
-export default OauthPage
+export default OauthPage;
