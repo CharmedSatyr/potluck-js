b7987eb5587bee337336fa5daaa7cbd033c2ecbb
Joseph Wolfe <joseph@WLF.to>
2025-01-03 03:49:49 -0800

    Remove unused utilities

--- src/utilities/format-start-date.ts
+++
@@ -1,9 +0,0 @@
-import { DateTime } from "luxon";
-
-export const formatStartDate = (startUtcMs: number) => {
-	return DateTime.fromMillis(startUtcMs).toLocaleString({
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-	});
-};
